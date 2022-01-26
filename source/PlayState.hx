d6f5a63868894cc40c74a686b7b9d9d5d84f804e
RaidenAlfares12 <97826418+RaidenAlfares12@users.noreply.github.com>
2022-01-26 20:27:33 +0700

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,4 +1,4 @@
-﻿package;
+package;
 
 #if desktop
 import Discord.DiscordClient;
@@ -1075,7 +1075,7 @@ class PlayState extends MusicBeatState
 		super.create();
 	}
 
-	public function addTextToDebug(text:String) {
+	public function addTextToDebug(text:String) {}
 		luaDebugGroup.forEachAlive(function(spr:DebugLuaText) {
 			spr.y += 20;
 		});
